07a982ee7df52adc964d79a66c1ce3e2e8c96b71
ChiiChii-1 <84898730+ChiiChii-1@users.noreply.github.com>
2021-08-12 09:42:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ import re
 import json
 from google.cloud import texttospeech
 
-prefix = os.getenv('DISCORD_BOT_PREFIX', default='🦑')
+prefix = os.getenv('DISCORD_BOT_PREFIX', default='$')
 tts_lang = os.getenv('DISCORD_BOT_LANG', default='ja-JP')
 tts_voice = os.getenv('DISCORD_BOT_VOICE', default='ja-JP-Wavenet-B')
 token = os.environ['DISCORD_BOT_TOKEN']
